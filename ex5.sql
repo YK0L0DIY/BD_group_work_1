60d5eee68e430f3d980f36d1a15ca40133c66cb2
yaroslav.kolodiy <ykolodiy17@gmail.com>
2018-10-25 14:04:19 +0100

    more info ex5

--- ex5.sql
+++ ex5.sql
@@ -20,3 +20,24 @@ select idcomp
 from  Especie natural inner join compartimento natural inner join Alojado natural inner join Animal
 where classe like 'Ave'
 
+--e--
+select IdComp
+from Especie natural inner join compartimento natural inner join Alojado natural inner join Animal 
+where Classe like 'Mamifero'
+Intersect
+select IdComp
+from Especie natural inner join compartimento natural inner join Alojado natural inner join Animal 
+where Classe like 'Reptil'
+
+--f--
+select NomeT
+from Tratador
+EXCEPT
+select distinct NomeT
+from compartimento natural inner join Tratador natural inner join Trata  
+where Tipo like 'Gaiola' or Tipo like 'Pantano'
+
+--g--
+select count(classe) as Numero_de_animais_mamiferos
+from Especie
+where classe like 'Mamifero'
