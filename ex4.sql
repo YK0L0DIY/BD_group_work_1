b2934a7241e6926dc734b43c93a7dd1bd28b7c66
Edfame <theedfame@gmail.com>
2018-10-28 11:00:09 +0000

    added ex4

---
+++ ex4.sql
@@ -0,0 +1,39 @@
+INSERT INTO tratador Values (345,'Yaroslav',1500,124);
+INSERT INTO Trata Values (10,345);
+INSERT INTO Compartimento Values (30,'Jaula');
+INSERT INTO Trata Values (30,345);
+
+INSERT INTO tratador Values (400,'Eduardo',1500,124);
+INSERT INTO Trata Values (8,400);
+INSERT INTO Trata Values (15,400);
+
+INSERT INTO Animal VALUES ('Fala Muito','masculino','Papagaio');
+INSERT INTO Alojado VALUES('Fala Muito',15);
+
+INSERT INTO Animal VALUES ('Blu','masculino','Arara');
+INSERT INTO Alojado VALUES('Blu',10);
+
+INSERT INTO Especie VALUES ('Leao','mamifero');
+INSERT INTO Animal VALUES ('Alex','masculino','Leao');
+INSERT INTO Compartimento VALUES (20,'savana');
+INSERT INTO Alojado VALUES ('Alex',20);
+
+INSERT INTO Animal VALUES ('Amália', 'feminino','Leao');
+INSERT INTO Alojado VALUES ('Amália',20);
+
+INSERT INTO Animal Values ('Milu','masculino','Tigre');
+INSERT INTO Alojado Values ('Milu',10);(edited);
+
+INSERT INTO Especie Values ('Macaco','Mamifero');
+INSERT INTO Animal Values ('Tiago','masculino','Macaco');
+INSERT INTO Alojado Values ('Tiago',30);
+
+INSERT INTO Animal Values ('Chupeta','masculino','Macaco');
+INSERT INTO Alojado Values ('Chupeta',30);
+
+INSERT INTO Especie Values ('Girafa','Mamifero');
+INSERT INTO Animal Values ('Manela','feminina','Girafa');
+INSERT INTO Alojado Values ('Manela',30);
+
+INSERT INTO Animal Values ('Melman','masculino','Girafa');
+INSERT INTO Alojado Values ('Melman',20);
